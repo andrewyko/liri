d7da4c281a52563207d1ec1dcc626aac91986ea1
Andrew Ko <andrewko@Andrews-MacBook-Pro.local>
2017-10-30 19:02:28 -0500

    update

---
+++ keys.js
@@ -0,0 +1,29 @@
+// console.log("");
+// console.log("The require has successfully loaded the twitter keys.js!")
+// console.log("");
+
+// exports.twitterKeys = {
+//   consumer_key: 'BxVh3G6IHIuoFJjQcRmh6nAJr',
+//   consumer_secret: 'FTltXmPSN2zncNxw86r5yUTXtwNepI5QCx4XQQg7t8mYiTE1S1',
+//   access_token_key: '1107657140-6wPTGV972Rs5EC21g8M7qfyiZAQHV0EcPQ4Qkn7',
+//   access_token_secret: 'LK6JJcsJRnyTY9L4FzRUu7VBfzSnERvIboe24w4sYFLNS',
+// }
+
+// exports.spotifykeys = {
+// 	id: '54d72841e6a940adb2b5bf919507c6f6',
+//   	secret: '1181e12735de4805b84ecf1705dd0352'
+// }
+
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'BxVh3G6IHIuoFJjQcRmh6nAJr',
+  consumer_secret: 'FTltXmPSN2zncNxw86r5yUTXtwNepI5QCx4XQQg7t8mYiTE1S1',
+  access_token_key: '1107657140-6wPTGV972Rs5EC21g8M7qfyiZAQHV0EcPQ4Qkn7',
+  access_token_secret: 'LK6JJcsJRnyTY9L4FzRUu7VBfzSnERvIboe24w4sYFLNS'
+};
+
+exports.spotifyKeys = {
+	  id: '54d72841e6a940adb2b5bf919507c6f6',
+  secret: '1181e12735de4805b84ecf1705dd0352'
+};
